1e78215c640e1e1db7abc981f070163d9d205d52
Tom Leese <tom@tomleese.me.uk>
2014-10-15 20:34:59 +0100

    Update to run chromium shell

--- app/src/main/java/org/studentrobotics/tablet/MainActivity.java
+++ app/src/main/java/org/studentrobotics/tablet/MainActivity.java
@@ -3,6 +3,7 @@ package org.studentrobotics.tablet;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
@@ -16,8 +17,17 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("http://152.78.205.153:8000"));
+            Intent intent;
+
+            if (isAppInstalled("org.chromium.chrome.shell", this)) {
+                intent = getPackageManager().getLaunchIntentForPackage("org.chromium.chrome.shell");
+            } else {
+                intent = new Intent(Intent.ACTION_VIEW);
+            }
+
+            // TODO: test for robot.sr or robot
+
+            intent.setData(Uri.parse("http://robot"));
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             finish();
@@ -25,6 +35,16 @@ public class MainActivity extends Activity {
             WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             manager.setWifiEnabled(true);
             manager.startScan();
+        }
+    }
+
+    private static boolean isAppInstalled(String name, Context context) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            pm.getPackageInfo(name, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
         }
     }
 
