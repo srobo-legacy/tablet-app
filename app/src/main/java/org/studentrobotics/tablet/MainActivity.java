57d8dbb570315f11b0b2b5a09acf3b5ce22e3aba
Tom Leese <tom@tomleese.me.uk>
2014-10-17 20:53:18 +0100

    Add dialogue for if tablet has no connection

--- app/src/main/java/org/studentrobotics/tablet/MainActivity.java
+++ app/src/main/java/org/studentrobotics/tablet/MainActivity.java
@@ -1,13 +1,16 @@
 package org.studentrobotics.tablet;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -15,21 +18,26 @@ import java.net.InetSocketAddress;
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     private class OpenBrowserTask extends AsyncTask<Void, Void, String> {
 
         @Override
         protected String doInBackground(Void... voids) {
             if (isServerAvailable("robot.sr")) {
                 return "robot.sr";
-            } else {
+            } else if (isServerAvailable("robot")) {
                 return "robot";
+            } else {
+                return null;
             }
         }
 
         private boolean isServerAvailable(String server) {
             try {
+                Log.d(TAG, "Trying robot at: " + server);
                 InetAddress ina = InetAddress.getByName(server);
-                return ina.isReachable(500);
+                return ina.isReachable(1000);
             } catch (IOException e) {
                 e.printStackTrace();
                 return false;
@@ -48,18 +56,30 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onPostExecute(String server) {
-            Intent intent;
-
-            if (isAppInstalled("org.chromium.chrome.shell", MainActivity.this)) {
-                intent = getPackageManager().getLaunchIntentForPackage("org.chromium.chrome.shell");
+            if (server == null) {
+                new AlertDialog.Builder(MainActivity.this)
+                        .setMessage("Please ensure the tablet is connected to the robot via WiFi and try again. If this problem persists, consult the docs or ask a question on the forum.")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .show();
             } else {
-                intent = new Intent(Intent.ACTION_VIEW);
-            }
+                Intent intent;
 
-            intent.setData(Uri.parse("http://" + server));
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            finish();
+                if (isAppInstalled("org.chromium.chrome.shell", MainActivity.this)) {
+                    intent = getPackageManager().getLaunchIntentForPackage("org.chromium.chrome.shell");
+                } else {
+                    intent = new Intent(Intent.ACTION_VIEW);
+                }
+
+                intent.setData(Uri.parse("http://" + server));
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
         }
     }
 
