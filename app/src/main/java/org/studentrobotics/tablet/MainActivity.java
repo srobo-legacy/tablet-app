6e40dce838ecdf4be4f33fa159b3f32d9d45db98
Tom Leese <tom@tomleese.me.uk>
2014-10-17 20:40:18 +0100

    Make tablet look for robot and robot.sr domains

--- app/src/main/java/org/studentrobotics/tablet/MainActivity.java
+++ app/src/main/java/org/studentrobotics/tablet/MainActivity.java
@@ -6,9 +6,62 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
 public class MainActivity extends Activity {
+
+    private class OpenBrowserTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            if (isServerAvailable("robot.sr")) {
+                return "robot.sr";
+            } else {
+                return "robot";
+            }
+        }
+
+        private boolean isServerAvailable(String server) {
+            try {
+                InetAddress ina = InetAddress.getByName(server);
+                return ina.isReachable(500);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        private boolean isAppInstalled(String name, Context context) {
+            PackageManager pm = context.getPackageManager();
+            try {
+                pm.getPackageInfo(name, PackageManager.GET_ACTIVITIES);
+                return true;
+            } catch (PackageManager.NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String server) {
+            Intent intent;
+
+            if (isAppInstalled("org.chromium.chrome.shell", MainActivity.this)) {
+                intent = getPackageManager().getLaunchIntentForPackage("org.chromium.chrome.shell");
+            } else {
+                intent = new Intent(Intent.ACTION_VIEW);
+            }
+
+            intent.setData(Uri.parse("http://" + server));
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,34 +70,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
-            Intent intent;
-
-            if (isAppInstalled("org.chromium.chrome.shell", this)) {
-                intent = getPackageManager().getLaunchIntentForPackage("org.chromium.chrome.shell");
-            } else {
-                intent = new Intent(Intent.ACTION_VIEW);
-            }
-
-            // TODO: test for robot.sr or robot
-
-            intent.setData(Uri.parse("http://robot"));
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-            finish();
+            new OpenBrowserTask().execute();
 
             WifiManager manager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
             manager.setWifiEnabled(true);
             manager.startScan();
-        }
-    }
-
-    private static boolean isAppInstalled(String name, Context context) {
-        PackageManager pm = context.getPackageManager();
-        try {
-            pm.getPackageInfo(name, PackageManager.GET_ACTIVITIES);
-            return true;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
         }
     }
 
